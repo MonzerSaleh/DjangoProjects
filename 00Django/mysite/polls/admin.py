fd4764cc0bb1263a38e289889a97870c8ebbd5e9
Monzer Saleh <36572937+MonzerSaleh@users.noreply.github.com>
2024-11-01 16:46:08 -0600

    Official django tutorial completed

--- 00Django/mysite/polls/admin.py
+++ 00Django/mysite/polls/admin.py
@@ -3,5 +3,27 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question,Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]    
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+
+# v2 - custom ordering
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ["pub_date", "question_text"]
+
+# v1 - question will now appear in admin panel
+# admin.site.register(Question)
--- Readme.md
+++ Readme.md
@@ -10,3 +10,5 @@ https://www.youtube.com/watch?v=RozHfswVZYE&list=PL4cUxeGkcC9iqfAag3a_BKEX1N43uJ
 
 DjangoReactTutorial
 - https://www.youtube.com/watch?v=c-QsfbznSXI&list=WL&index=2&t=333s&ab_channel=TechWithTim
+
+
